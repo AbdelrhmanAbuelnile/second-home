747287e7dc3412d6a720a7f0aa8ce10dbc024064
unknown <abdohani362@gmail.com>
2024-02-07 05:11:41 +0200

    student page

--- src/Pages/CustomerPage.jsx
+++ src/Pages/CustomerPage.jsx
@@ -1,31 +1,31 @@
 import { useLocation } from 'react-router-dom';
-import customers from '../data/customers.json';
+import students from '../data/students.json';
 
 function CustomerPage() {
   const location = useLocation();
-  let customerId = location.pathname.split('/').pop();
-  let customer = customers.find((customer) => customer.id === parseInt(customerId));
+  let studentID = location.pathname.split('/').pop();
+  let student = students.find((student) => student.student_id.toLowerCase() === studentID.toLowerCase());
 
-  if (!customer) {
-    return <div>Customer not found</div>;
+  if (!student) {
+    return <div>student not found</div>;
   }
 
   return (
     <div className="text-third mt-40">
-      <h1 className="text-2xl font-semibold mb-4 border-b border-secondary">{customer.customerName}</h1>
+      <h1 className="text-2xl font-semibold mb-4 border-b border-secondary">{student.student_name}</h1>
       <div className="grid grid-cols-2 gap-4">
         <div>
           <h2 className="text-lg font-semibold mb-2">Contact Information</h2>
-          <p>Email: <span className='text-secondary'>{customer.customerEmail}</span></p>
-          <p>Phone Number: <span className='text-secondary'>{customer.phoneNumber}</span></p>
+          <p>Email: <span className='text-secondary'>{student.student_email}</span></p>
+          <p>Phone Number: <span className='text-secondary'>{student.student_number}</span></p>
         </div>
         <div>
-          <h2 className="text-lg font-semibold mb-2">Order Information</h2>
-          <p>Fridge Model: <span className='text-secondary'>{customer.fridge_model}</span></p>
-          <p>Location: <span className='text-secondary'>{customer.location}</span></p>
-          <p>Number of Items: <span className='text-secondary'>{customer.number_of_items}</span></p>
-          <p>Total: <span className='text-secondary'>${customer.total}</span></p>
-          <p>Date: <span className='text-secondary'>{customer.date}</span></p>
+          <h2 className="text-lg font-semibold mb-2">Student Information</h2>
+          <p>Student Location: <span className='text-secondary'>{student.student_location}</span></p>
+          <p>Preferred Location: <span className='text-secondary'>{student.preferred_location}</span></p>
+          <p>rooms needed: <span className='text-secondary'>{student.num_rooms_needed}</span></p>
+          <p>Max Budget: <span className='text-secondary'>${student.max_budget}</span></p>
+          <p>Student ID: <span className='text-secondary'>{student.student_id}</span></p>
         </div>
       </div>
     </div>
